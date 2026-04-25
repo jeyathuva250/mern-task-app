e6bfd7829a51e5f27b5b659fb0bece01a1ecd2bb
Jeyanthan Thuvarakan <jeyathuva25@gmail.com>
2026-04-25 21:43:37 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api/tasks";
+  const API_URL = import.meta.env.VITE_API_URL || "https://mern-task-app-hi43.onrender.com/";
 
   const fetchTasks = async () => {
     setLoading(true);
